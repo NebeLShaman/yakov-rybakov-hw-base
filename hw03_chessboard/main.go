de36fb0dd6a011df096813cb5d120edcadbe553b
Yakov Rybakov <yakov.rybakov@axenix.pro>
2023-08-24 15:44:19 +0300

    hw03_completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Place your code here.
+	var (
+		hashtag     string = "[#]"
+		spacex      string = "[ ]"
+		lenghtBoard        = 8
+		heightBoard        = 8
+	)
+
+	for j := 1; j <= heightBoard; j++ {
+		for i := 1; i <= lenghtBoard; i++ {
+			if i%2 == 0 && j%2 == 0 {
+				fmt.Printf("%s", hashtag)
+			} else if i%2 != 0 && j%2 != 0 {
+				fmt.Printf("%s", hashtag)
+			} else {
+				fmt.Printf("%s", spacex)
+			}
+			if i == lenghtBoard {
+				fmt.Printf("%s", "\n")
+			}
+		}
+	}
 }
