02fd32454e452cb03c5095e03dbb7cb9f71bc248
Yakov Rybakov <yakov.rybakov@axenix.pro>
2023-08-25 15:53:52 +0300

    fixed path to module

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/fixme_my_friend/hw02_fix_app/printer"
-	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
+	"github.com/NebeLShaman/yakov-rybakov-hw-base/hw02_fix_app/printer"
+	"github.com/NebeLShaman/yakov-rybakov-hw-base/hw02_fix_app/reader"
+	"github.com/NebeLShaman/yakov-rybakov-hw-base/hw02_fix_app/types"
 )
 
 func main() {
