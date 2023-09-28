55d6519641647218ef6e173a934ddf14fc450dd6
Yakov Rybakov <yakov.rybakov@axenix.pro>
2023-09-29 00:05:46 +0300

    solved in accordance with the task

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-type BookComparerMode string
-
+// Book - структура для представления информации о книге
 type Book struct {
 	id     int
 	title  string
@@ -15,26 +13,8 @@ type Book struct {
 	size   int
 	rate   float64
 }
-type BookComparer struct {
-	Mode string
-}
 
-func main() {
-	book1 := NewBook(1, "1984", "Оруэлл Дж.", 1945, 352, 4.6)
-	book2 := NewBook(2, "Animal Farm: A Fairy Story", "George Orwell", 1949, 986, 4.0)
-
-	var comparerMode string
-	fmt.Printf("Введите один из критериев для сравнения:\nByYear, BySize, ByRate\n")
-	fmt.Fscan(os.Stdin, &comparerMode)
-	comparer := &BookComparer{Mode: comparerMode}
-
-	if comparer.Compare(book1, book2) {
-		fmt.Printf("%s больше чем %s", book1.title, book2.title)
-	} else {
-		fmt.Printf("%s больше чем %s", book2.title, book1.title)
-	}
-}
-
+// NewBook - конструктор для создания новой книги
 func NewBook(id int, title, author string, year, size int, rate float64) *Book {
 	return &Book{
 		id:     id,
@@ -46,15 +26,91 @@ func NewBook(id int, title, author string, year, size int, rate float64) *Book {
 	}
 }
 
+// Сеттеры для возможности утсновки значения поля
+func (b *Book) SetTitle(title string) {
+	b.title = title
+}
+
+func (b *Book) SetAuthor(author string) {
+	b.author = author
+}
+
+func (b *Book) SetYear(year int) {
+	b.year = year
+}
+
+func (b *Book) SetSize(size int) {
+	b.size = size
+}
+
+func (b *Book) SetRate(rate float64) {
+	b.rate = rate
+}
+
+// Геттеры для возможности получения значения поля
+
+func (b *Book) GetID() int {
+	return b.id
+}
+
+func (b *Book) GetTitle() string {
+	return b.title
+}
+
+func (b *Book) GetAuthor() string {
+	return b.author
+}
+
+func (b *Book) GetYear() int {
+	return b.year
+}
+
+func (b *Book) GetSize() int {
+	return b.size
+}
+
+func (b *Book) GetRate() float64 {
+	return b.rate
+}
+
+// BookComparerMode - перечисление для выбора режима сравнения
+type BookComparerMode int
+
+const (
+	ByYear BookComparerMode = iota
+	BySize
+	ByRate
+)
+
+// BookComparer - структура для сравнения книг по выбранному полю
+type BookComparer struct {
+	Mode BookComparerMode
+}
+
+// Compare - метод для сравнения двух книг по выбранному полю
 func (c *BookComparer) Compare(book1, book2 *Book) bool {
 	switch c.Mode {
-	case "ByYear":
-		return book1.year > book2.year
-	case "BySize":
+	case ByYear:
+		return book1.year < book2.year
+	case BySize:
 		return book1.size > book2.size
-	case "ByRate":
+	case ByRate:
 		return book1.rate > book2.rate
 	default:
 		return false
+	}
+}
+
+func main() {
+	book1 := NewBook(1, "1984", "Оруэлл Дж.", 1945, 352, 4.6)
+	book2 := NewBook(2, "Animal Farm: A Fairy Story", "George Orwell", 1949, 986, 4.0)
+	// Создаем объект BookComparer с выбранным режимом сравнения
+	comparer := &BookComparer{Mode: BySize}
+
+	// Сравниваем книги по выбранному полю
+	if comparer.Compare(book1, book2) {
+		fmt.Println("Book 1 is greater than Book 2")
+	} else {
+		fmt.Println("Book 2 is greater than Book 1")
 	}
 }
